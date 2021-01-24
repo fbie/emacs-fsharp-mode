0ccfc84a47eb29cb55bf746fc5e6e79e6c3a9478
Florian Biermann <fbie@itu.dk>
2021-01-24 18:09:31 +0100

    Use project-files (plural)

--- fsharp-mode.el
+++ fsharp-mode.el
@@ -358,7 +358,7 @@ folders relative to DIR-OR-FILE."
 
 ;; Make project.el aware of fsharp projects
 (defun fsharp-mode-project-root (dir)
-  (-when-let (project-file (fsharp-mode/find-sln-or-fsproj dir))
+  (-when-let (project-files (fsharp-mode/find-sln-or-fsproj dir))
     (cons 'fsharp (file-name-directory project-file))))
 
 (cl-defmethod project-roots ((project (head fsharp)))
